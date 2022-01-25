0418312c69a65aa9595b7afa382eaed3c6a8274c
Teacher A <teaching.know@gmail.com>
2022-01-25 09:44:52 +0100

    Update 05_timer.rb

--- lib/05_timer.rb
+++ lib/05_timer.rb
@@ -2,6 +2,19 @@
 
 
 
-def time_string(v)
-    return Time.at(v).utc.strftime("%H:%M:%S")
-end
+def time_string(nb_sec)
+  heure = (nb_sec/3600)
+  min = (nb_sec%3600)/60 #le reste de l'opération nb_sec/3600 est alors divisé par 60
+  sec = ((nb_sec%3600)%60)
+  
+  if heure<10
+    heure = "0#{heure}"
+  end
+  if min<10
+    min = "0#{min}"
+  end
+  if sec<10
+    sec = "0#{sec}"
+  end
+  return "#{heure}:#{min}:#{sec}"
+  end
